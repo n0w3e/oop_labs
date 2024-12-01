d8cb7139d0ac289f35d4fcafb65d1afc049633bd
n0w3e <5ejyjvdldwfk@mail.ru>
2024-12-01 16:11:37 +0300

    maked change

--- lab4/include/Array.h
+++ lab4/include/Array.h
@@ -1,42 +1,69 @@
 #pragma once
-#include <concepts>
-#include <vector>
-#include <memory>
 #include "Figure.h"
-
-template <typename T>
-concept Scalar = std::is_arithmetic_v<T>;
+#include <memory>
+#include <stdexcept>
 
 template <Scalar T>
 class Array {
 public:
-    void add(std::shared_ptr<Figure<T>> figure) {
-        figures.push_back(figure);
+    Array(size_t initial_capacity = 4)
+        : capacity(initial_capacity), size(0), data(new std::shared_ptr<Figure<T>>[initial_capacity]) {}
+
+    ~Array() {
+        delete[] data;
+    }
+
+    void add(const std::shared_ptr<Figure<T>>& figure) {
+        if (size == capacity) {
+            resize(capacity * 2);
+        }
+        data[size++] = figure;
     }
 
     void remove(size_t index) {
-        if (index < figures.size()) {
-            figures.erase(figures.begin() + index);
+        if (index >= size) {
+            throw std::out_of_range("Index out of range");
         }
+        for (size_t i = index; i < size - 1; ++i) {
+            data[i] = data[i + 1];
+        }
+        --size;
     }
 
-    T getTotalArea() const {
-        T totalArea = 0;
-        for (const auto& figure : figures) {
-            totalArea += figure->getArea();
+    std::shared_ptr<Figure<T>>& operator[](size_t index) {
+        if (index >= size) {
+            throw std::out_of_range("Index out of range");
         }
-        return totalArea;
+        return data[index];
     }
 
-    void printDetails() const {
-        for (size_t i = 0; i < figures.size(); ++i) {
-            auto center = figures[i]->getCenter();
-            std::cout << "Figure " << i + 1 << ": Center (" << center->getX()
-                      << ", " << center->getY() << "), Area " << figures[i]->getArea()
-                      << "\n";
+    const std::shared_ptr<Figure<T>>& operator[](size_t index) const {
+        if (index >= size) {
+            throw std::out_of_range("Index out of range");
         }
+        return data[index];
+    }
+
+    size_t get_size() const {
+        return size;
+    }
+
+    size_t get_capacity() const {
+        return capacity;
     }
 
 private:
-    std::vector<std::shared_ptr<Figure<T>>> figures;
+    size_t capacity;
+    size_t size;
+    std::shared_ptr<Figure<T>>* data;
+
+    void resize(size_t new_capacity) {
+        std::shared_ptr<Figure<T>>* new_data = new std::shared_ptr<Figure<T>>[new_capacity];
+        for (size_t i = 0; i < size; ++i) {
+            new_data[i] = data[i];
+        }
+        delete[] data;
+        data = new_data;
+        capacity = new_capacity;
+    }
 };
